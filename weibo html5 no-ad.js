877b6fcfb41889c263f0b45c9ea54bdbfb14fa92
RecluseWind <22146920+RecluseWind@users.noreply.github.com>
2020-07-24 18:29:37 +0800

    Update weibo html5 no-ad.js

--- weibo html5 no-ad.js
+++ weibo html5 no-ad.js
@@ -1,16 +1,17 @@
 // ==UserScript==
 // @name         微博 HTML5版 去除广告
 // @namespace    https://github.com/RecluseWind/userjs
-// @version      0.1.2
+// @version      0.1.3
 // @description  weibo html5 no-ad
 // @author       RecluseWind
 // @match        https://m.weibo.cn/*
 // @include      https://m.weibo.cn/*
 // @grant        none
 // @require      https://code.jquery.com/jquery-3.3.1.min.js
+// @updateURL    https://raw.githubusercontent.com/RecluseWind/userjs/master/weibo%20html5%20no-ad.js
 //@license       MIT
 // ==/UserScript==
-//参考https://blog.csdn.net/weixin_43897573/article/details/106462979
+//参考：https://blog.csdn.net/weixin_43897573/article/details/106462979
 jQuery.noConflict();
 (function($) {
     'use strict';
@@ -38,5 +39,4 @@ jQuery.noConflict();
             }, 1000);
         };
     });
-    // Your code here...
 })(jQuery);
