5456c5f6d68d58a64b4a5e98698c6e8c4b60342f
RecluseWind <22146920+RecluseWind@users.noreply.github.com>
2020-07-29 11:27:42 +0800

    Update weibo-html5-noad.js
    增加判断微博类型

--- src/weibo-html5-noad.js
+++ src/weibo-html5-noad.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         微博 HTML5版 去除广告
 // @namespace    https://github.com/RecluseWind/userjs
-// @version      0.1.4
+// @version      0.1.5
 // @description  weibo html5 no-ad
 // @author       RecluseWind
 // @match        https://m.weibo.cn/*
@@ -26,14 +26,19 @@ jQuery.noConflict();
             }
             refresh();
         }, 1000);
-		//清理广告方法
+	//清理广告微博
         function refresh(){
             setTimeout(function(){
-            //判断每个div.wb-item-wrap,是否有后代元素div.m-ctrl-box
+                //判断每个div.wb-item-wrap,是否有后代元素div.m-ctrl-box
                 $("div.wb-item-wrap").each(function() {
                     if ($(this).find("div.m-ctrl-box").length>0) {
-                        $(this).remove();
-                        //console.log("清除广告微博成功");
+                        let text=$(this).find("h4").html();
+                        //console.log(text);
+                        //判断广告微博类型进行屏蔽，避免误伤"粉丝可见"
+                        const blockList = ["广告","分享","粉丝头条"];
+                        if(blockList.indexOf(text)>-1){
+                            $(this).remove();
+                        }
                     }
                 })
             }, 1000);
